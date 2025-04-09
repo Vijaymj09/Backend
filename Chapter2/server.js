a47b42f70bbda8fe6d6a5885bacecbee3f3585c7
Vijaymj09 <karthimj09@gmail.com>
2025-04-09 08:31:42 +0530

    add GET route for root path in server.js

--- Chapter2/server.js
+++ Chapter2/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+//HTTP VERBS && Routes(or paths)
+app.get("/", (req, res) => {
+  console.log("GET request received at /", req.method);
+  res.sendStatus(200);
 });
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+);
